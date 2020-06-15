0dc95338ddf822265585c82ce767592a0e607090
unknown <shaikhaala@Live.com>
2020-06-15 10:29:51 -0700

    did the task

--- calculator.js
+++ calculator.js
@@ -1,3 +1,14 @@
 // Define the numbers and the operation here
-
+const x = 5;
+const y = 3;
+const operator = "/";
 // Write if-statements here
+if (operator === "+") {
+  console.log(x + y);
+} else if (operator === "-") {
+  console.log(x - y);
+} else if (operator === "*") {
+  console.log(x * y);
+} else if (operator === "/") {
+  console.log(x / y);
+}
